76e69219faccd85df90a337568b68cb3e2e89952
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-05 13:43:59 +0530

    Deleted 3D old files

--- portfolio/src/Components/3D/settings.jsx
+++
@@ -1,6 +0,0 @@
-export const transition = {
-    type: "spring",
-    duration: 0.7,
-    bounce: 0.2
-  };
-  
--- portfolio/src/Components/3D/use-smooth-transform.jsx
+++
@@ -1,5 +0,0 @@
-import { useSpring, useTransform } from "framer-motion";
-
-export function useSmoothTransform(value, springOptions, transformer) {
-  return useSpring(useTransform(value, transformer), springOptions);
-}
