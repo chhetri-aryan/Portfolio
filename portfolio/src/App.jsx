bd223c9f32c52c9a703ee7f9b96c65b74ec757b2
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-09 13:56:03 +0530

    no major fixes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -19,7 +19,6 @@ const Body = styled.div`
 const Wrapper = styled.div`
   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
 `
 
 function App() {
