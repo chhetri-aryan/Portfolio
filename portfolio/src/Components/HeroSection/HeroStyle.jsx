640266709217e6ab8a12f64c277946645b903d5f
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-11 23:06:27 +0530

    removed unused styles

--- portfolio/src/Components/HeroSection/HeroStyle.jsx
+++ portfolio/src/Components/HeroSection/HeroStyle.jsx
@@ -133,26 +133,6 @@ export const Img = styled.img`
   }
 `;
 
-export const Img3D = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  max-width: 400px;
-  max-height: 400px;
-  // border-radius: 50%;
-  // border: 2px solid ${({ theme }) => theme.primary};
-
-  @media (max-width: 768px) {
-    max-width: 400px;
-    max-height: 400px;
-  }
-
-  @media (max-width: 640px) {
-    max-width: 280px;
-    max-height: 280px;
-  }
-`;
-
 export const Title = styled.div`
   font-weight: 700;
   font-size: 50px;
@@ -195,7 +175,7 @@ export const SubTitle = styled.div`
   font-size: 20px;
   line-height: 32px;
   margin-bottom: 42px;
-  color: ${({ theme }) => theme.text_primary + 95};
+  color: ${({ theme }) => theme.text_primary + 99};
 
   @media (max-width: 960px) {
     text-align: center;
