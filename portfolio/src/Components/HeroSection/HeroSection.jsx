a473e69deb8d5035bf7571bb5e4d3ad52d1f4a39
Aryan Chhetri <aryanjr53@gmail.com>
2024-07-12 14:33:07 +0530

    fixed spline resolution for mid and small

--- portfolio/src/Components/HeroSection/HeroSection.jsx
+++ portfolio/src/Components/HeroSection/HeroSection.jsx
@@ -11,8 +11,8 @@ const HeroSection = () => {
         <div id="about" className=''>
             <HeroContainer>
                 <HeroInnerContainer >
-                    <div className='grid lg:grid-cols-2 sm:grid-cols-1 lg:gap-32'>
-                        <div id="Left" className='order-2 justify-center mt-2'>
+                    <div className='grid lg:grid-cols-2 sm:grid-cols-1 lg:gap-32 mt-16 md:mt-20 lg:mt-0'>
+                        <div id="Left" className='order-2 justify-center mt-60 md:mt-40 lg:mt-5'>
                             <Title>Hi, I am <br /> {Bio.name}</Title>
                                 <TextLoop className='flex flex-wrap'>
                                     I am a
@@ -33,13 +33,11 @@ const HeroSection = () => {
                         </div>
 
 
-                        <div id="Right" className='lg:order-2 sm:order-1 lg:-m-56'>
-
-                            <Spline
-                                scene="https://prod.spline.design/UTFMrrw71-cpURyo/scene.splinecode"
-                                // className=' sm:h-96 sm:w-96 lg:h-96 lg:w-96 md:h-96 md:w-96 '
-                            />
-                        </div>
+                        <div id="Right" className='lg:order-2 sm:order-1 -m-56 h-92 '>
+              <Spline
+                scene="https://prod.spline.design/UTFMrrw71-cpURyo/scene.splinecode"
+              />
+            </div>
 
                     </div>
                 </HeroInnerContainer>
